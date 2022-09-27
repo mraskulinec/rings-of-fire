73f062acd836b2350745687d17d79de5136dff06
Abe <abe.gdev@gmail.com>
2022-09-27 02:02:53 -0400

    main code commit

---
+++ src/Creative1/C1/ExampleScenes/ParticleScene/particles/ParticleSystem2DModel.ts
@@ -0,0 +1,54 @@
+import {
+    AParticle,
+    ASerializable,
+    BasicParticleSystem2DModel,
+    Color,
+    Mat3,
+    V2,
+    Vec2,
+    VertexArray2D
+} from "../../../../../anigraph";
+import {Particle} from "./Particle";
+
+enum AppStateKeys{
+    ExampleParticleParam="ExampleParticleParam"
+}
+
+@ASerializable("ParticleSystem2DModel")
+export class ParticleSystem2DModel extends BasicParticleSystem2DModel{
+    static AppStateKeys = AppStateKeys;
+
+    exampleParticleParam:number=1
+
+    nParticles = 10;
+    get particles(): Particle[] {
+        return this._particles as Particle[];
+    };
+
+    getTransformForParticle(p: Particle): Mat3 {
+        return Mat3.Translation2D(p.position).times(Mat3.Scale2D(p.radius));
+    }
+
+    init(): void {
+        for(let i=0;i<this.nParticles;i++){
+            this.particles.push(new Particle(i, Vec2.Random([-0.5,0.5]), 1, Color.Random()))
+        }
+    }
+
+
+    getColorForParticle(p: Particle): Color {
+        return p.color;
+    }
+
+
+    timeUpdate(t:number){
+        for(let p of this.particles){
+            p.position = V2(Math.cos(t+p.id), Math.sin(t+p.id)).times(this.exampleParticleParam);
+        }
+        this.signalParticleUpdate();
+    }
+
+
+
+}
+
---
+++ src/TODO.md
@@ -0,0 +1,6 @@
+# To Do
+
+- Decide what the model, view, and control patterns should be
+    - View Delegate Interfrace?
+    -
+- Port old AniGraph camera class by removing dependencies
